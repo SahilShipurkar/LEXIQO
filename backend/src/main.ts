54a2c146f512de770405a0f68ea558afb86547b4
SahilShipurkar <sahilshipurkar88@gmail.com>
2026-02-17 14:47:09 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,14 +6,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'https://lexiqo.netlify.app',
-      'http://localhost:5173',
-    ],
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    credentials: true,
-  });
+ app.enableCors({
+  origin: 'https://lexiqo.netlify.app', 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+});
 
   // Enable DTO validation (recommended)
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
